08e09b51d13b099514e072dcb19ffd0c3afb1aa5
iftakherone <54303402+iftakherone@users.noreply.github.com>
2022-01-18 19:19:53 +0600

    updating footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Col, Row } from 'react-bootstrap';
+import { Container, Col, Row } from 'react-bootstrap';
 import GitHubButton from 'react-github-btn';
 
 const Footer = () => {
@@ -7,9 +7,11 @@ const Footer = () => {
         <footer className="pt-5">
             <Row className="d-flex flex-column justify-content-center align-items-center">
                 <Col sm={4} md={4} lg={2} className="d-flex justify-content-evenly mx-auto">
-                    <GitHubButton href="https://github.com/iftakherhossen" aria-label="Follow @iftakherhossen on GitHub">Follow Me</GitHubButton>
-                    <span className="mx-sm-0 mx-md-4"> </span>
-                    <GitHubButton href="https://github.com/iftakherhossen/tic-tac-toe" data-icon="octicon-star" aria-label="Star iftakherhossen/tic-tac-toe on GitHub">Star</GitHubButton>
+                    <Container>
+                        <GitHubButton href="https://github.com/iftakherhossen" aria-label="Follow @iftakherhossen on GitHub">Follow Me</GitHubButton>
+                        <span className="mx-sm-0 mx-md-4"> </span>
+                        <GitHubButton href="https://github.com/iftakherhossen/tic-tac-toe" data-icon="octicon-star" aria-label="Star iftakherhossen/tic-tac-toe on GitHub">Star</GitHubButton>
+                    </Container>
                 </Col>
                 <Col sm={12} md={9} lg={6} className="text-center mt-2">
                     <p>© 2022, All rights reserved By <a href="https://iftakher-hossen.vercel.app/" target="_blank" rel="noreferrer" className="coloredTxt text-decoration-none">Iftakher Hossen</a></p>
